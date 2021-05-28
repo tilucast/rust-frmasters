4938b660bdbb7de90d102d0abc3b2f5b2b7b8559
Lucas <tilucast@gmail.com>
2021-05-28 19:00:16 -0300

    part5

--- part5/src/main.rs
+++ part5/src/main.rs
@@ -1,12 +1,11 @@
 fn main() {
     let numbers = vec![2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24];
 
-    let sum_of_nums = sum(numbers);
-    let product_of_nums = 0; // 👉 TODO call product() and put answer here
-    let average_of_nums = 0; // 👉 TODO call average() and put answer here
+    let (numbers, sum_of_nums) = sum(numbers);
+    let (numbers, average_of_nums) = average(numbers);
+    let (_, product_of_nums) = product(numbers);
 
     // 💡 TIP: You'll get a compile error. Here are two ways you can fix it:
-    //
     // Option 1: Pass numbers.clone() some of the time.
     //           (Experiment to see when it's needed!)
     //
@@ -22,28 +21,29 @@ fn main() {
     println!("Average of these numbers: {}", average_of_nums);
 }
 
-fn sum(numbers: Vec<i64>) -> i64 {
+fn sum(numbers: Vec<i64>) -> (Vec<i64>, i64) {
     let mut total = 0;
 
     for num in numbers.iter() {
         total += num;
     }
 
-    total
+    (numbers, total)
 }
 
-fn product(numbers: Vec<i64>) -> i64 {
+fn product(numbers: Vec<i64>) -> (Vec<i64>, i64) {
     let mut total = 1;
 
     for num in numbers.iter() {
         total *= num;
     }
 
-    total
+    (numbers, total)
 }
 
-fn average(numbers: Vec<i64>) -> i64 {
+fn average(numbers: Vec<i64>) -> (Vec<i64>, i64) {
     let length = numbers.len() as i64;
+    let (numbers, sum) = sum(numbers);
 
-    sum(numbers) / length
+    (numbers, sum / length)
 }
